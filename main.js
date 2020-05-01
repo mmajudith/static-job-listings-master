953cbaa4e24ba465a8f2e8b549d77f5986748f8b
mma judith <mmajudith2000@gmail.com>
2020-04-30 20:28:52 -0700

    Job Listings

--- main.js
+++ main.js
@@ -1,35 +1,40 @@
 'use strict';
 
+// Variables Declarations
 const filContainer = document.querySelector('.filter');
 const clear = document.querySelector('.clear'); 
 
-let filWrapper = document.querySelector('#fill');
+let filWrapper = document.querySelector('.fill');
 let close = './images/icon-remove.svg';
 
 let items = document.querySelectorAll('li');
 let changeToArr = Array.from(items);
 
+
 const generateTemp =  (data) =>{
-        
-    let html = `<span class="text">${data}</span> 
-                <img src="${close}" alt="remove" class="remove">
-                
+
+    let html = `
+                <ul class="con">
+                  <li class="text">${data}</li>
+                  <img src="${close}" alt="remove" class="remove">
+                </ul> 
                 `
     return html;
 }
 
-let filteredRoles = changeToArr.filter(item =>{
-    item.addEventListener('click', e =>{
+// filtered the roles class
+changeToArr.filter(el =>{
+    el.addEventListener('click', e =>{
         if(e.target.classList.contains('role')){
             filContainer.style.display = 'block';
             filWrapper.innerHTML += generateTemp(e.target.innerHTML);
         }
     });
-   
 });
 
-let filteredLevel = changeToArr.filter(item =>{
-    item.addEventListener('click', e =>{
+// filtered the levels class
+changeToArr.filter(el =>{
+    el.addEventListener('click', e =>{
         if(e.target.classList.contains('level')){
             filContainer.style.display = 'block';
             filWrapper.innerHTML += generateTemp(e.target.innerHTML);
@@ -37,8 +42,9 @@ let filteredLevel = changeToArr.filter(item =>{
     });
 });
 
-let filteredLang = changeToArr.filter(item =>{
-    item.addEventListener('click', e =>{
+// filtered the languages class
+changeToArr.filter(el =>{
+    el.addEventListener('click', e =>{
         if(e.target.classList.contains('language')){
             filContainer.style.display = 'block';
             filWrapper.innerHTML += generateTemp(e.target.innerHTML);
@@ -46,8 +52,9 @@ let filteredLang = changeToArr.filter(item =>{
     });
 });
 
-let filteredtools = changeToArr.filter(item =>{
-    item.addEventListener('click', e =>{
+// filtered the tools class
+changeToArr.filter(el =>{
+    el.addEventListener('click', e =>{
         if(e.target.classList.contains('tool')){
             filContainer.style.display = 'block';
             filWrapper.innerHTML += generateTemp(e.target.innerHTML);
@@ -55,8 +62,22 @@ let filteredtools = changeToArr.filter(item =>{
     });
 });
 
+
+
 clear.addEventListener('click', ()=>{
     filContainer.style.display = 'none';
     filWrapper.innerHTML = '';
     
 });
+
+
+
+filWrapper.addEventListener('click', e =>{
+
+     if(e.target.classList.contains('remove')){
+          e.target.parentElement.remove();
+    }
+    
+});
+
+
